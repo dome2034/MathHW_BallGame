c82e824578d5d6685e65e5316b4a06ecbc8ba3c7
Thanakrit <dome2034@gmail.com>
2017-11-10 11:38:24 +0700

    add 2.2, 2.3

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ for(let i =0;i<8;i++){
 for(let i =0;i<12;i++){
     BallBox.push('b');
 }
-let RoundToPlay = 10000;
-let win = 0;
+let RoundToPlay = 100000; //all n
+let NFor2_2 = 0, NFor2_3 = 0;
+let win = 0, win2_2 = 0, win2_3 = 0;
 
 for(let i = 0; i< RoundToPlay;i++){ // loop play game
     game = new Game();
@@ -16,13 +17,27 @@ for(let i = 0; i< RoundToPlay;i++){ // loop play game
     game.shuffleBall();
     game.pickBall();
     game.getPickedBall();
-    if(game.isWin()){
+    if(game.isWin()){ // count win for 2.1
         win++;
     }
-    console.log(win);
+    if(game.isFirst5For2_2()){
+        NFor2_2++;
+        if(game.isWin()){ // count win for 2.2
+            win2_2++;
+        }
+    }
+    if(game.isFirst5For2_3()){
+        NFor2_3++;
+        if(game.isWin()){ // count win for 2.3
+            win2_3++;
+        }
+    }
+
     game = null;
     delete game;
 }
 
 console.log("------ Result -------");
-console.log("2.1 Probability is " + (win/RoundToPlay)); //probability result.
+console.log("2.1 Probability is " + (win/RoundToPlay)); //probability result 2.1
+console.log("2.2 Probability is " + (win2_2/NFor2_2)); //probability result 2.2
+console.log("2.3 Probability is " + (win2_3/NFor2_3)); //probability result 2.3
--- game.js
+++ game.js
@@ -26,11 +26,12 @@ function game() {
     }
     this.pickBall = function(){ //pick 10 ball.
         BallPicked = BallBox.slice(9, 19);
+        console.log(BallPicked);
     }
     this.isWin = function(){ //check win or not.
         let j = 0;
         let Black = 0;
-        let win = true;
+        let Win = true;
         for(let i = 0;i< (BallPicked.length+2);i++){ // loop 12 round.
             if(i==10){ //if this is round 10 go to first ball.
                 j=0;
@@ -42,12 +43,50 @@ function game() {
                 Black = 0;
             }
             if(Black == 3){ //if black is 3, exit loop
-                win = false;
+                Win = false;
                 break;
             }
             j++;
         }
-        return win;
+        return Win;
+    }
+    this.isFirst5For2_2 = function() { //check condition of 2.2
+        let Black = 0;
+        let Pass = true;
+        let First5 = BallPicked.slice(0, 5);
+        console.log(First5);
+        for(let i = 0;i<First5.length;i++){
+            if(First5[i] == 'b'){ //if this is black, increase black.
+                Black++;
+            }
+            else{ //if this is white, reset black.
+                Black = 0;
+            }
+            if(Black == 3){ //if black is 3, exit loop
+                Pass = false;
+                break;
+            }
+        }
+        return Pass;
+    }
+    this.isFirst5For2_3 = function() { //check condition of 2.3
+        let White = 0;
+        let Pass = false;
+        let First5 = BallPicked.slice(0, 5);
+        console.log(First5);
+        for(let i = 0;i<First5.length;i++){
+            if(First5[i] == 'w'){ //if this is white, increase white.
+                White++;
+            }
+            else{ //if this is black, reset white.
+                White = 0;
+            }
+            if(White == 5){ //if white is 5, exit loop
+                Pass = true;
+                break;
+            }
+        }
+        return Pass;
     }
 };
 
