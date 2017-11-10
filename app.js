99e88eafd7ed0a3da16346fd5946bdca86a100ab
Thanakrit <dome2034@gmail.com>
2017-11-10 09:20:43 +0700

    add Homework 2.1

--- app.js
+++ app.js
@@ -7,13 +7,22 @@ for(let i =0;i<8;i++){
 for(let i =0;i<12;i++){
     BallBox.push('b');
 }
+let RoundToPlay = 10000;
+let win = 0;
 
-game = new Game();
-game.setBox(BallBox);
-game.shuffleBall();
-game.pickBall();
+for(let i = 0; i< RoundToPlay;i++){ // loop play game
+    game = new Game();
+    game.setBox(BallBox);
+    game.shuffleBall();
+    game.pickBall();
+    game.getPickedBall();
+    if(game.isWin()){
+        win++;
+    }
+    console.log(win);
+    game = null;
+    delete game;
+}
 
-console.log(game.getPickedBall());
-
-game = null;
-delete game;
+console.log("------ Result -------");
+console.log("2.1 Probability is " + (win/RoundToPlay)); //probability result.
--- game.js
+++ game.js
@@ -15,7 +15,7 @@ function game() {
     }
 
 //-------------- Method -------------------------------------
-    this.shuffleBall = function() {
+    this.shuffleBall = function() { //shuffle ball in box.
         let j, x, i;
         for (i = BallBox.length - 1; i > 0; i--) {
             j = Math.floor(Math.random() * (i + 1));
@@ -24,10 +24,31 @@ function game() {
             BallBox[j] = x;
         }
     }
-    this.pickBall = function(){
+    this.pickBall = function(){ //pick 10 ball.
         BallPicked = BallBox.slice(9, 19);
     }
-    
+    this.isWin = function(){ //check win or not.
+        let j = 0;
+        let Black = 0;
+        let win = true;
+        for(let i = 0;i< (BallPicked.length+2);i++){ // loop 12 round.
+            if(i==10){ //if this is round 10 go to first ball.
+                j=0;
+            }
+            if(BallPicked[j] == 'b'){ //if this is black, increase black.
+                Black++;
+            }
+            else{ //if this is white, reset black.
+                Black = 0;
+            }
+            if(Black == 3){ //if black is 3, exit loop
+                win = false;
+                break;
+            }
+            j++;
+        }
+        return win;
+    }
 };
 
 module.exports = game; 
